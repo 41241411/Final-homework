92118dedb21e7224926c2a0e96c5c759a675e562
41241411 游恩綺 <49340402+41241411@users.noreply.github.com>
2025-01-08 05:22:45 +0800

    測試<script src="js/main.js" defer></script>是否還有問題

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// 動態載入 Navbar 和 Footer
+async function loadComponent(containerId, filePath) {
+    const container = document.getElementById(containerId);
+    if (container) {
+        const response = await fetch(filePath);
+        const content = await response.text();
+        container.innerHTML = content;
+    }
+}
+
+// 載入 Navbar 和 Footer
+loadComponent('navbar-container', '/navbar.html');
+loadComponent('footer-container', '/footer.html');
